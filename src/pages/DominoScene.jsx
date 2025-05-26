40bc01e05cd6c9a18bfda28091d925aeb3f7706a
임승현 <96limshyun@gmail.com>
2025-05-26 22:48:50 +0900

    Feature: 캔버스 초기 설정
    [FEATURE] 초기 캔버스 설정 #8

--- src/pages/DominoScene.jsx
+++ src/pages/DominoScene.jsx
@@ -1,5 +1,35 @@
+import { RoundedBox, OrbitControls, Sky } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
 const DominoScene = () => {
-  return <div>DominoScene</div>;
+  return (
+    <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
+      <ambientLight
+        intensity={0.5}
+        color="white"
+      />
+      <directionalLight
+        castShadow
+        position={[5, 10, 5]}
+      />
+      <Sky
+        distance={450000}
+        sunPosition={[0, 1, 0]}
+        inclination={0}
+        azimuth={0.25}
+      />
+      <OrbitControls />
+
+      <RoundedBox>
+        <meshStandardMaterial color="orange" />
+      </RoundedBox>
+
+      <mesh position={[2, 2, 2]}>
+        <boxGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+    </Canvas>
+  );
 };
 
 export default DominoScene;
