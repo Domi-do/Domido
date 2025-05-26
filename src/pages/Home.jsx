c2884ca1ee2a16fc9c7a6fb823e34f4982af76fe
moonin1228 <80632139+moonin1228@users.noreply.github.com>
2025-05-26 21:18:06 +0900

    Feature: 초기화면 & Not Found UI 구현  (#5)
    - 초기 진입 화면 구성
    - Not Found(404) 전용 페이지 제작
    - 라우터에 NotFound 페이지 등록
    - 게임 화면과의 경로 분리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,30 @@
+import { Link } from "react-router-dom";
+
+import logo from "@/assets/images/logo.png";
+import startButton from "@/assets/images/start_button.png";
+
 const Home = () => {
-  return <div className="text-3xl font-bold underline">Home</div>;
+  return (
+    <section className="relative w-full h-screen bg-[url('./assets/images/intro_bg.jpg')] bg-no-repeat bg-center bg-cover">
+      <h1 className="absolute top-[10%] left-1/2 transform -translate-x-1/2 max-w-[700px] w-full">
+        <img
+          src={logo}
+          alt="DOMINO"
+          draggable="false"
+        />
+      </h1>
+      <Link
+        to="/game"
+        className="block absolute left-1/2 bottom-[20%] transform -translate-x-1/2 w-[60%] max-w-[300px] cursor-pointer"
+      >
+        <img
+          src={startButton}
+          alt="시작버튼"
+          draggable="false"
+        />
+      </Link>
+    </section>
+  );
 };
 
 export default Home;
