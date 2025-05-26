0e5947e366655fd08e3d21764b2d4c2455748de4
moonin1228 <[moonin1228@gmail.com]>
2025-05-26 20:29:41 +0900

    feature: Not Found UI 구현
    #4

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 
 import DominoScene from "@/pages/DominoScene";
 import Home from "@/pages/Home";
+import NotFound from "@/pages/NotFound";
 
 const routes = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "game", element: <DominoScene /> },
   { path: "projects/:projectId", element: <DominoScene /> },
+  { path: "*", element: <NotFound /> },
 ]);
 
 export default routes;
