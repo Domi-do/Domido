c103b6ce47f1e45f0e2f98ab2cd9feda2f046a52
임승현 <96limshyun@gmail.com>
2025-05-26 20:43:14 +0900

    feat: 외부 영역 클릭 감지용 useOnClickOutSide 커스텀 훅 추가
    [FEATURE] 전역 Portal 시스템 구축 (React createPortal) #3

---
+++ src/hooks/useOnClickOutSide.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+const useOnClickOutSide = (ref, onClickOutSide) => {
+  useEffect(() => {
+    const handleClose = (event) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      onClickOutSide();
+    };
+
+    document.addEventListener("mousedown", handleClose);
+
+    return () => document.removeEventListener("mousedown", handleClose);
+  }, [ref, onClickOutSide]);
+};
+
+export default useOnClickOutSide;
