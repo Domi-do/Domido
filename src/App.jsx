dbf91b27184da080782826e0e3574ba7402b63d9
moonin1228 <[moonin1228@gmail.com]>
2025-05-26 14:24:44 +0900

    config: husky 설정
    #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import React from "react";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div id="wrap">
+      <h1>hello world</h1>
+      <p> badly formatted jsx </p>
+    </div>
+  );
 };
-
 export default App;
