3fcbc96e9867101e36deaa114890c848447fa1b3
임승현 <126482821+96limshyun@users.noreply.github.com>
2025-05-27 23:25:40 +0900

    Feature: 사이드바 패널 구현 (#18)
    - public 파일에 3D GLB 파일 및 썸네일 이미지 추가
    - 오브젝트 절대경로 객체로 상수화
    - 사이드 패널 UI 구현

--- src/components/DominoHUD/DominoHUD.jsx
+++ src/components/DominoHUD/DominoHUD.jsx
@@ -1,17 +1,22 @@
+import SidePanel from "@/components/DominoHUD/SidePanel/SidePanel";
+
 const DominoHUD = ({ rotationSensitivity, onChangeSensitivity }) => {
   return (
-    <div className="fixed z-50">
-      <input
-        id="sensitivity"
-        type="range"
-        min={1}
-        max={50}
-        step={0.01}
-        value={rotationSensitivity}
-        onChange={onChangeSensitivity}
-        className="w-full"
-      />
-    </div>
+    <>
+      <div className="fixed z-50">
+        <input
+          id="sensitivity"
+          type="range"
+          min={1}
+          max={50}
+          step={0.01}
+          value={rotationSensitivity}
+          onChange={onChangeSensitivity}
+          className="w-full"
+        />
+      </div>
+      <SidePanel />
+    </>
   );
 };
 
