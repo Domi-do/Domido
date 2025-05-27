6d7b692f1c4d2efea2579bbdf155d8402f2588dd
임승현 <96limshyun@gmail.com>
2025-05-27 23:11:25 +0900

    Feature: 오브젝트 절대경로 객체로 상수화

---
+++ src/constants/objectPaths.js
@@ -0,0 +1,22 @@
+export const OBJECT_PATHS = {
+  STATIC_OBJECTS: {
+    slide: { thumbnail: "/images/slide.png", model: "/objects/slide.glb" },
+    spiralStairs: { thumbnail: "/images/spiral_stairs.png", model: "/objects/spiral_stairs.glb" },
+    staircase: { thumbnail: "/images/staircase.png", model: "/objects/staircase.glb" },
+    stairsClosedShort: {
+      thumbnail: "/images/stairs_closed_short.png",
+      model: "/objects/stairs_closed_short.glb",
+    },
+    stairsClosed: { thumbnail: "/images/stairs_closed.png", model: "/objects/stairs_closed.glb" },
+    stairs: { thumbnail: "/images/stairs.png", model: "/objects/stairs.glb" },
+  },
+  DYNAMIC_OBJECTS: {
+    beachBall: { thumbnail: "/images/beach_ball.png", model: "/objects/beach_ball.glb" },
+    cannon: { thumbnail: "/images/cannon.png", model: "/objects/cannon.glb" },
+    pokeball: { thumbnail: "/images/pokeball.png", model: "/objects/pokeball.glb" },
+    soccerFootball: {
+      thumbnail: "/images/soccer_football.png",
+      model: "/objects/soccer_football.glb",
+    },
+  },
+};
