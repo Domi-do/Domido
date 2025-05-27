a4f624e41edf74ffdc9fa7060982e5d3c4ed04e4
moonin1228 <80632139+moonin1228@users.noreply.github.com>
2025-05-27 15:09:34 +0900

    Feature: 카메라 이동 구현
    Feature: 카메라 이동 구현
    
    -WASD 키를 통한 카메라 이동 기능 추가
    -OribitContorls을 이용한 회전 기능 추가
    
    Refactor: PR 피드백 반영
    
    - 함수 네이밍 개선
    - 관련 로직 커스텀 훅으로 분리
    - 주석에 설명 추가하여 가독성 향상

--- src/pages/DominoScene.jsx
+++ src/pages/DominoScene.jsx
@@ -1,34 +1,61 @@
-import { RoundedBox, OrbitControls, Sky } from "@react-three/drei";
+import { RoundedBox, Sky, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
+
+import CameraControls from "@/components/CameraControls/CameraControls";
 
 const DominoScene = () => {
+  const [rotationSensitivity, setRotationSensitivity] = useState(1);
+
+  const handleRotationSensitivity = (e) => {
+    setRotationSensitivity(e.target.value);
+  };
+
   return (
-    <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
-      <ambientLight
-        intensity={0.5}
-        color="white"
+    <>
+      <input
+        id="sensitivity"
+        type="range"
+        min={1}
+        max={50}
+        step={0.01}
+        value={rotationSensitivity}
+        onChange={handleRotationSensitivity}
+        className="w-full"
       />
-      <directionalLight
-        castShadow
-        position={[5, 10, 5]}
-      />
-      <Sky
-        distance={450000}
-        sunPosition={[0, 1, 0]}
-        inclination={0}
-        azimuth={0.25}
-      />
-      <OrbitControls />
+      <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
+        <ambientLight
+          intensity={0.5}
+          color="white"
+        />
+        <directionalLight
+          castShadow
+          position={[5, 10, 5]}
+        />
+        {/* Sky 컴포넌트가 렌더링 성능에 영향을 줘 일시적으로 비활성화함 (최적화 후 재사용 예정) */}
+        {/* <Sky
+          distance={450000}
+          sunPosition={[0, 1, 0]}
+          inclination={0}
+          azimuth={0.25}
+        /> */}
+        <CameraControls />
+        <OrbitControls
+          enableZoom={true}
+          mouseButtons={{ LEFT: null, MIDDLE: 0, RIGHT: null }}
+          rotateSpeed={rotationSensitivity}
+        />
 
-      <RoundedBox>
-        <meshStandardMaterial color="orange" />
-      </RoundedBox>
+        <RoundedBox>
+          <meshStandardMaterial color="orange" />
+        </RoundedBox>
 
-      <mesh position={[2, 2, 2]}>
-        <boxGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
-    </Canvas>
+        <mesh position={[2, 2, 2]}>
+          <boxGeometry />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+      </Canvas>
+    </>
   );
 };
 
