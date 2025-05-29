8c86f5bf4ac2e11b16c3b485ca81e7bbea1f20f4
moonin1228 <[moonin1228@gmail.com]>
2025-05-29 09:45:51 +0900

    Refactor: JSX 기반 UI로 전환
    #20

--- src/components/DominoHUD/GuideToast/GuideToast.jsx
+++ src/components/DominoHUD/GuideToast/GuideToast.jsx
@@ -1,13 +1,19 @@
-import guide from "/public/images/guide_ui.png";
-
 const GuideToast = () => {
   return (
-    <div>
-      <img
-        src={guide}
-        alt="GUIDE"
-        className="fixed bottom-[20px] left-1/2 -translate-x-1/2 z-50 pointer-events-none w-[300px]"
-      />
+    <div className="fixed bottom-5 left-1/2 -translate-x-1/2 z-50 flex items-center bg-[#1a1a1a] text-gray-100 px-6 py-5 rounded-full shadow-md gap-3 opacity-80">
+      <div className="flex items-center gap-2">
+        <div className="w-8 h-8 rounded-full bg-white text-black flex items-center justify-center font-bold text-lg">
+          X
+        </div>
+        <span className="text-base font-light">Remove</span>
+      </div>
+      <div className="h-5 w-px bg-gray-500 opacity-40" />
+      <div className="flex items-center gap-2">
+        <div className="w-8 h-8 rounded-full bg-white text-black flex items-center justify-center font-bold text-lg">
+          H
+        </div>
+        <span className="text-base font-light">Hide</span>
+      </div>
     </div>
   );
 };
