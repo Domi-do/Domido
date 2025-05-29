400c4f7db8236facf3dc1d45d28c11a1c73fe720
eunseozz <94948796+eunseozz@users.noreply.github.com>
2025-05-29 13:19:54 +0900

    Feature: 도미노 시뮬레이션 기능 구현 (#24)
    - Feature: 손가락 버튼 UI 및 커서 변경 기능 구현
    - Feature: 도미노 시뮬레이션 기능 구현
    - Fix: 로직 통합
    - feature/object/placement -> feature/pointer-push-object 로직 통합

--- src/components/DominoHUD/DominoHUD.jsx
+++ src/components/DominoHUD/DominoHUD.jsx
@@ -1,25 +1,54 @@
+import playButton from "/images/play_button.png";
+import stopButton from "/images/stop_button.png";
+
 import SidePanel from "@/components/DominoHUD/SidePanel/SidePanel";
 
 const DominoHUD = ({
   rotationSensitivity,
   onChangeSensitivity,
+  simulationMode,
+  countdownNumber,
+  updateSimulationState,
   selectedObject,
   setSelectedObject,
 }) => {
+  const isSimulating = simulationMode === "SIMULATING";
+
+  const buttonConfig = {
+    icon: isSimulating ? stopButton : playButton,
+    nextMode: isSimulating ? "EDIT" : "READY",
+  };
+
   return (
     <>
-      <div className="fixed z-50">
-        <input
-          id="sensitivity"
-          type="range"
-          min={1}
-          max={50}
-          step={0.01}
-          value={rotationSensitivity}
-          onChange={onChangeSensitivity}
-          className="w-full"
+      <input
+        id="sensitivity"
+        type="range"
+        min={1}
+        max={50}
+        step={0.01}
+        value={rotationSensitivity}
+        onChange={onChangeSensitivity}
+        className="fixed z-50 bottom-0 w-full"
+      />
+
+      <button
+        className="fixed z-50 cursor-pointer w-[60px] h-[60px]"
+        onClick={() => updateSimulationState(buttonConfig.nextMode)}
+      >
+        <img
+          src={buttonConfig.icon}
+          alt={buttonConfig.alt}
+          draggable="false"
         />
-      </div>
+      </button>
+
+      {simulationMode === "COUNTDOWN" && (
+        <span className="fixed z-50 text-[200px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-bold">
+          {countdownNumber}
+        </span>
+      )}
+
       <SidePanel
         selectedObject={selectedObject}
         setSelectedObject={setSelectedObject}
