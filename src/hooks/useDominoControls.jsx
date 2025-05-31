1d86e6b0f4e0c595c1907648709d95549d91eab6
moonin1228 <80632139+moonin1228@users.noreply.github.com>
2025-05-31 20:17:36 +0900

    Feature: Undo & Clear 기능 구현 (#42)
    - Feature: Undo 버튼 클릭 또는 단축키 U 입력 시 히스토리 한 단계 되돌리기
    - Feature: Reset 버튼 클릭 시 도미노 전체 삭제

--- src/hooks/useDominoControls.jsx
+++ src/hooks/useDominoControls.jsx
@@ -1,33 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import useDominoStore from "@/store/useDominoStore";
 
 const useDominoControls = (onToggleGuideToast) => {
-  const { dominos, setDominos, selectedDominoKey } = useDominoStore();
+  const { dominos, setDominos, selectedDominoKey, setSelectedDominoKey } = useDominoStore();
+  const historys = useRef([]);
+  const prevLengthRef = useRef(dominos.length);
 
   useEffect(() => {
-    const pressX = (e) => {
-      if (e.key.toLowerCase() === "x") {
-        const updateDominos = [...dominos].filter((dominos) => dominos.id !== selectedDominoKey);
-
+    const pressX = (event) => {
+      if (event.key.toLowerCase() === "x") {
+        historys.current.push([...dominos]);
+        const updateDominos = dominos.filter((domino) => domino.id !== selectedDominoKey);
         setDominos(updateDominos);
-        setTimeout(() => {
-          onToggleGuideToast(false);
-        }, 100);
+        setSelectedDominoKey(null);
+        setTimeout(() => onToggleGuideToast(false), 100);
       }
     };
 
-    const pressH = (e) => {
-      if (e.key.toLowerCase() === "h") {
-        const updatedDominos = dominos.map((item) => {
-          const currentDominoIds = item.id;
-          if (currentDominoIds === selectedDominoKey) {
-            const isTransparent = item.opacity < 1;
-            return { ...item, opacity: isTransparent ? 1 : 0.3 };
-          }
-          return item;
-        });
-
+    const pressH = (event) => {
+      if (event.key.toLowerCase() === "h") {
+        historys.current.push([...dominos]);
+        const updatedDominos = dominos.map((item) =>
+          item.id === selectedDominoKey ? { ...item, opacity: item.opacity === 1 ? 1 : 0.3 } : item,
+        );
         setDominos(updatedDominos);
         onToggleGuideToast(false);
       }
@@ -35,12 +31,33 @@ const useDominoControls = (onToggleGuideToast) => {
 
     window.addEventListener("keydown", pressX);
     window.addEventListener("keydown", pressH);
-
     return () => {
       window.removeEventListener("keydown", pressX);
       window.removeEventListener("keydown", pressH);
     };
-  }, [selectedDominoKey]);
+  }, [selectedDominoKey, dominos]);
+
+  useEffect(() => {
+    if (dominos.length > prevLengthRef.current) {
+      historys.current.push([...dominos]);
+    }
+    prevLengthRef.current = dominos.length;
+  }, [dominos]);
+
+  useEffect(() => {
+    const pressU = (event) => {
+      if (historys.current.length < 0) return;
+      if (event.key.toLowerCase() === "u") {
+        historys.current.pop();
+        if (historys.current.length >= 1) {
+          setDominos(historys.current[historys.current.length - 1]);
+        }
+      }
+    };
+
+    window.addEventListener("keydown", pressU);
+    return () => window.removeEventListener("keydown", pressU);
+  }, []);
 };
 
 export default useDominoControls;
