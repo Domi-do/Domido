6bedd0584bcd0247a3af010216eb9b201efd8383
임승현 <96limshyun@gmail.com>
2025-06-01 15:13:08 +0900

    Refactor: AudioController에 play 메서드 추가 및 init 분리로 사운드 재생 유연성 향상
    [FEATURE]  도미노 배치 시 효과음 재생 기능 추가 #46

--- src/utils/audioController.js
+++ src/utils/audioController.js
@@ -4,19 +4,23 @@ class AudioController {
   #listener = null;
   #sound = null;
 
-  init(camera, volumeLevel, isLoop = true) {
+  init(camera, volumeLevel, isLoop = false) {
     if (this.#listener || this.#sound) return;
 
     this.#listener = new THREE.AudioListener();
     camera.add(this.#listener);
 
     this.#sound = new THREE.Audio(this.#listener);
+    this.#sound.setLoop(isLoop);
+    this.#sound.setVolume(volumeLevel);
+  }
+
+  play(src) {
+    if (!this.#sound) return;
 
     const loader = new THREE.AudioLoader();
-    loader.load("/sounds/bgm.mp3", (buffer) => {
+    loader.load(src, (buffer) => {
       this.#sound.setBuffer(buffer);
-      this.#sound.setLoop(isLoop);
-      this.#sound.setVolume(volumeLevel);
       this.#sound.play();
     });
   }
