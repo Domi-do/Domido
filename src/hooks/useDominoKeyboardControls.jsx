d0c083d566cf1dc0cbb1ec56015cfeddbc1f9477
임승현 <96limshyun@gmail.com>
2025-06-01 17:03:05 +0900

    refactor: 도미노 키보드 제어 로직 (x : 삭제 기능) useDominoKeyboardControls 훅으로 분리

---
+++ src/hooks/useDominoKeyboardControls.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef } from "react";
+
+import useDominoStore from "@/store/useDominoStore";
+
+const useDominoKeyboardControls = (onToggleGuideToast) => {
+  const { setDominos, setSelectedDominoKey } = useDominoStore();
+  const historys = useRef([]);
+  // const prevLengthRef = useRef(dominos.length);
+
+  const deleteSelectedDomino = () => {
+    const { dominos, selectedDominoKey } = useDominoStore.getState();
+    if (!selectedDominoKey) return;
+
+    historys.current.push([...dominos]);
+    const updatedDominos = dominos.filter((domino) => domino.id !== selectedDominoKey);
+    setDominos(updatedDominos);
+    setSelectedDominoKey(null);
+    setTimeout(() => onToggleGuideToast(false), 100);
+  };
+
+  const keyMap = {
+    x: deleteSelectedDomino,
+    q: () => console.log("q"),
+    e: () => console.log("e"),
+    u: () => console.log("u"),
+    escape: () => console.log("Escape"),
+  };
+
+  const handleKeydown = (event) => {
+    event.stopPropagation();
+
+    const key = event.key.toLowerCase();
+    const keyboardHandler = keyMap[key];
+    if (typeof keyboardHandler === "function") {
+      keyboardHandler(event);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeydown);
+    return () => window.removeEventListener("keydown", handleKeydown);
+  }, []);
+
+  return;
+};
+
+export default useDominoKeyboardControls;
