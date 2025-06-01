2b94bc828dfae4737dc25e441b4172abf33404ce
eunseozz <eunseolovely@gmail.com>
2025-06-01 17:52:30 +0900

    Feature: Suspense fallback을 이용한 로딩 화면 구현
    #44

--- src/components/DominoCanvas/DominoCanvas.jsx
+++ src/components/DominoCanvas/DominoCanvas.jsx
@@ -1,36 +1,40 @@
 import { Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Physics } from "@react-three/rapier";
+import { Suspense } from "react";
 
 import CameraControls from "@/components/CameraControls/CameraControls";
 import CursorFollowerObject from "@/components/CursorFollowerObject/CursorFollowerObject";
 import GlobalAudio from "@/components/GlobalAudio/GlobalAudio";
+import Loading from "@/components/Loading/Loading";
 
 const DominoCanvas = ({ rotationSensitivity, children }) => {
   return (
-    <Canvas camera={{ position: [0, 5, 5], fov: 75 }}>
-      <GlobalAudio />
-      <ambientLight
-        color="white"
-        intensity={1}
-      />
-      <directionalLight
-        castShadow
-        intensity={1}
-        position={[5, 10, 5]}
-      />
-      {/* <Environment
-        preset="park"
-        background
-      /> */}
-      <CameraControls rotationSensitivity={rotationSensitivity} />
-      <Physics>
-        <>
-          <CursorFollowerObject />
-          {children}
-        </>
-      </Physics>
-    </Canvas>
+    <>
+      <Canvas camera={{ position: [0, 5, 5], fov: 75 }}>
+        <Suspense fallback={<Loading />}>
+          <GlobalAudio />
+          <ambientLight
+            color="white"
+            intensity={1}
+          />
+          <directionalLight
+            castShadow
+            intensity={1}
+            position={[5, 10, 5]}
+          />
+          <Environment
+            preset="park"
+            background
+          />
+          <CameraControls rotationSensitivity={rotationSensitivity} />
+          <Physics>
+            <CursorFollowerObject />
+            {children}
+          </Physics>
+        </Suspense>
+      </Canvas>
+    </>
   );
 };
 
