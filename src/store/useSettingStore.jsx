5372a2bcc527b2201539fca942758cac9f4604c7
임승현 <96limshyun@gmail.com>
2025-06-03 00:04:20 +0900

    Feat: 도미노 배치 시 효과음 볼륨 조절 기능 추가
    [BUG] 게임 화면에서 새로고침시 배경음악이 재생되지 않는 현상 #55

--- src/store/useSettingStore.jsx
+++ src/store/useSettingStore.jsx
@@ -4,10 +4,12 @@ const useSettingStore = create((set) => ({
   groundType: "wood_dark",
   rotationSensitivity: 1,
   volumeLevel: 0.5,
+  objectVolume: 3,
 
   setGroundType: (groundType) => set(() => ({ groundType })),
   setRotationSensitivity: (sensitivity) => set(() => ({ rotationSensitivity: sensitivity })),
   setVolumeLevel: (newVolume) => set(() => ({ volumeLevel: newVolume })),
+  setObjectVolume: (newVolume) => set(() => ({ objectVolume: newVolume })),
 }));
 
 export default useSettingStore;
