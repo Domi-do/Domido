e493b6c6771170373f4f7a6e85b5058d733c9f69
moonin1228 <moonin1228@gmail.com>
2025-06-03 13:15:32 +0900

    Feature: 전구, 스위치 오브젝트 구현
    #58

--- src/pages/DominoScene.jsx
+++ src/pages/DominoScene.jsx
@@ -1,4 +1,6 @@
 import { RigidBody } from "@react-three/rapier";
+import { CuboidCollider } from "@react-three/rapier";
+import { useState } from "react";
 
 import { Ground, ObjectRenderer, DominoCanvas } from "@/components/DominoCanvas";
 import DominoHUD from "@/components/DominoHUD/DominoHUD";
@@ -11,6 +13,7 @@ import useSettingStore from "@/store/useSettingStore";
 const DominoScene = () => {
   const dominos = useDominoStore((state) => state.dominos);
   const rotationSensitivity = useSettingStore((state) => state.rotationSensitivity);
+  const [isLightOn, setLightOn] = useState(false);
 
   const { isOpenGuideToastVisible, openGuideToast, closeGuideToast, setIsGuideToastVisible } =
     useToastControls();
@@ -28,30 +31,71 @@ const DominoScene = () => {
       />
       <DominoCanvas rotationSensitivity={rotationSensitivity}>
         {dominos.length
-          && dominos.map((domino, index) => (
-            <RigidBody
-              colliders={domino.objectInfo.paths.colliders}
-              name="domino"
-              key={domino.id}
-              restitution={0}
-              friction={1}
-              linearDamping={0.01}
-              angularDamping={0.01}
-              position={domino.position}
-              rotation={domino.rotation}
-              ref={(ref) => (rigidBodyRefs.current[index] = ref)}
-            >
-              <ObjectRenderer
-                dominoInfo={domino.objectInfo}
-                key={domino.id}
-                onPointerOver={(event) => openGuideToast(event, domino.id)}
-                onPointerOut={closeGuideToast}
-                onClick={(event) => readyDominoSimulation(event, index)}
-                opacity={domino.opacity}
-                color={domino.color}
-              />
-            </RigidBody>
-          ))}
+          && dominos.map((domino, index) => {
+            const { position, rotation, color, opacity, id } = domino;
+            const { colliders, name } = domino.objectInfo.paths;
+            return (
+              <RigidBody
+                colliders={colliders}
+                name={name}
+                key={id}
+                restitution={0}
+                friction={1}
+                linearDamping={0.01}
+                angularDamping={0.01}
+                position={position}
+                rotation={rotation}
+                ref={(ref) => (rigidBodyRefs.current[index] = ref)}
+                type={name === "lightbulb" ? "fixed" : "dynamic"}
+              >
+                {name === "lightbulb" && (
+                  <>
+                    <CuboidCollider
+                      args={[0.3, 0.4, 0.4]}
+                      position={[1.65, -0.5, 1.25]}
+                      sensor
+                      onIntersectionEnter={() => {
+                        setTimeout(() => {
+                          setLightOn(true);
+                        }, 300);
+                      }}
+                    />
+                    <mesh position={[0, 0.1, -0.2]}>
+                      <boxGeometry args={[1, 0.5, 0.4]} />
+                      <meshStandardMaterial
+                        color={isLightOn ? "white" : "gray"}
+                        emissive={isLightOn ? "rgb(255,255,150)" : "black"}
+                        emissiveIntensity={50}
+                        metalness={0.1}
+                        roughness={0.3}
+                        transparent
+                        opacity={0}
+                      />
+                      {isLightOn && (
+                        <pointLight
+                          position={[0, -0.5, 0]}
+                          color="yellow"
+                          intensity={30}
+                          distance={2}
+                          decay={1}
+                        />
+                      )}
+                    </mesh>
+                  </>
+                )}
+
+                <ObjectRenderer
+                  dominoInfo={domino.objectInfo}
+                  key={id}
+                  onPointerOver={(event) => openGuideToast(event, id)}
+                  onPointerOut={closeGuideToast}
+                  onClick={(event) => readyDominoSimulation(event, index)}
+                  opacity={opacity}
+                  color={color}
+                />
+              </RigidBody>
+            );
+          })}
         <Ground />
       </DominoCanvas>
     </>
