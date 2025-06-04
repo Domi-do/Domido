97a3a1f7c3fe7b39932f3e7cfcc701919d868665
eunseozz <94948796+eunseozz@users.noreply.github.com>
2025-06-04 10:28:38 +0900

    Feature: 대포 오브젝트 관련 애니메이션 구현 (#63)
    - Feature: 대포 발사 시 지정된 오브젝트를 앞 방향으로 튕겨내는 애니메이션 구현
    - beachBall, pokeball, soccerFootball 등 대상에 반응
    - 지정된 위치로 이동 후 속도 부여해 발사 효과 처리
    - 1초 후 재등장 및 물리 엔진 재활성화
    - Fix: 대포 오브젝트 바닥 아래로 추락하는 현상 수정

--- src/pages/DominoScene.jsx
+++ src/pages/DominoScene.jsx
@@ -1,7 +1,8 @@
-import { RigidBody } from "@react-three/rapier";
+import { RigidBody, CuboidCollider } from "@react-three/rapier";
 
 import { Ground, ObjectRenderer, DominoCanvas } from "@/components/DominoCanvas";
 import DominoHUD from "@/components/DominoHUD/DominoHUD";
+import useCannonControls from "@/hooks/useCannonControls";
 import useDominoKeyboardControls from "@/hooks/useDominoKeyboardControls";
 import useDominoSimulation from "@/hooks/useDominoSimulation";
 import useToastControls from "@/hooks/useToastControls";
@@ -16,6 +17,7 @@ const DominoScene = () => {
     useToastControls();
 
   const { rigidBodyRefs, readyDominoSimulation, switchToReadyMode } = useDominoSimulation();
+  const { handleCannonTrigger } = useCannonControls();
 
   useDominoKeyboardControls(setIsGuideToastVisible);
 
@@ -28,30 +30,45 @@ const DominoScene = () => {
       />
       <DominoCanvas rotationSensitivity={rotationSensitivity}>
         {dominos.length
-          && dominos.map((domino, index) => (
-            <RigidBody
-              colliders={domino.objectInfo.paths.colliders}
-              name="domino"
-              key={domino.id}
-              restitution={0}
-              friction={1}
-              linearDamping={0.01}
-              angularDamping={0.01}
-              position={domino.position}
-              rotation={domino.rotation}
-              ref={(ref) => (rigidBodyRefs.current[index] = ref)}
-            >
-              <ObjectRenderer
-                dominoInfo={domino.objectInfo}
+          && dominos.map((domino, index) => {
+            return (
+              <RigidBody
+                type={domino.objectInfo.paths.type}
+                colliders={domino.objectInfo.paths.colliders}
+                name={domino.objectInfo.objectName}
                 key={domino.id}
-                onPointerOver={(event) => openGuideToast(event, domino.id)}
-                onPointerOut={closeGuideToast}
-                onClick={(event) => readyDominoSimulation(event, index)}
-                opacity={domino.opacity}
-                color={domino.color}
-              />
-            </RigidBody>
-          ))}
+                restitution={0}
+                friction={1}
+                linearDamping={0.01}
+                angularDamping={0.01}
+                position={domino.position}
+                rotation={domino.rotation}
+                ref={(ref) => (rigidBodyRefs.current[index] = ref)}
+              >
+                {domino.objectInfo?.objectName === "cannon" && (
+                  <>
+                    <CuboidCollider
+                      args={[0.2, 0.7, 0.2]}
+                      position={[0, 0, -1]}
+                      sensor
+                      onIntersectionEnter={({ other, target }) => {
+                        handleCannonTrigger(other, target);
+                      }}
+                    />
+                  </>
+                )}
+
+                <ObjectRenderer
+                  dominoInfo={domino.objectInfo}
+                  onPointerOver={(event) => openGuideToast(event, domino.id)}
+                  onPointerOut={closeGuideToast}
+                  onClick={(event) => readyDominoSimulation(event, index)}
+                  opacity={domino.opacity}
+                  color={domino.color}
+                />
+              </RigidBody>
+            );
+          })}
         <Ground />
       </DominoCanvas>
     </>
