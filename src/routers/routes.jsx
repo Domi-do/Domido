c0d1a3b199b8d57b563ca09c1d29d86b0b757b68
moonin1228 <moonin1228@gmail.com>
2025-06-05 16:40:53 +0900

    Feature: 카카오 로그인 및 인가 콜백 라우트 추가
    - /oauth/login 경로에 KakaoLogin 컴포넌트 연결
    - /oauth/callback 경로에 OAuthCallback 컴포넌트 연결
    
    #75

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,14 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import KakaoLogin from "@/components/KakaoLogin";
 import DominoScene from "@/pages/DominoScene";
 import Home from "@/pages/Home";
 import NotFound from "@/pages/NotFound";
+import OAuthCallback from "@/pages/OAuthCallback";
 
 const routes = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "game", element: <DominoScene /> },
   { path: "projects/:projectId", element: <DominoScene /> },
   { path: "*", element: <NotFound /> },
+  { path: "/oauth/login", element: <KakaoLogin /> },
+  { path: "/oauth/callback", element: <OAuthCallback /> },
 ]);
 
 export default routes;
