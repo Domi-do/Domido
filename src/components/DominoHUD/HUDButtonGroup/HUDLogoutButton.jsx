5ec0dddb7f58afcea316192fd6ebcd0d268ee0cf
eunseozz <eunseolovely@gmail.com>
2025-06-10 09:40:06 +0900

    refactor: HUD 버튼 구조 개선 및 로그아웃 버튼 컴포넌트로 분리
    #104

---
+++ src/components/DominoHUD/HUDButtonGroup/HUDLogoutButton.jsx
@@ -0,0 +1,20 @@
+import { BiLogOut } from "react-icons/bi";
+
+import useDominoStore from "@/store/useDominoStore";
+
+const HUDLogoutButton = ({ onLogout }) => {
+  const setSelectedDomino = useDominoStore((state) => state.setSelectedDomino);
+
+  return (
+    <button
+      onMouseOver={() => setSelectedDomino(null)}
+      onClick={onLogout}
+      className="flex items-center gap-2 px-4 py-2 bg-sky-300 text-gray-800 text-sm font-semibold rounded-full shadow-md hover:bg-sky-400 transition-all cursor-pointer"
+    >
+      <BiLogOut className="text-lg" />
+      로그아웃
+    </button>
+  );
+};
+
+export default HUDLogoutButton;
