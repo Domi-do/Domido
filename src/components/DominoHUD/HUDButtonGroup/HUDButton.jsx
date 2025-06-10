82c2f2807e19cf76bce21e8316351b0f5f780375
moonin1228 <moonin1228@gmail.com>
2025-06-10 20:46:42 +0900

    Merge branch 'feature/project-invite-and-switch' into develop

--- src/components/DominoHUD/HUDButtonGroup/HUDButton.jsx
+++ src/components/DominoHUD/HUDButtonGroup/HUDButton.jsx
@@ -1,6 +1,6 @@
 import useDominoStore from "@/store/useDominoStore";
 
-const HUDButton = ({ onClick, className, children }) => {
+const HUDButton = ({ onClick, children }) => {
   const setSelectedDomino = useDominoStore((state) => state.setSelectedDomino);
 
   return (
@@ -8,7 +8,7 @@ const HUDButton = ({ onClick, className, children }) => {
       <button
         onMouseOver={() => setSelectedDomino(null)}
         onClick={onClick}
-        className={className}
+        className="w-12 h-12 flex items-center justify-center bg-white text-gray-700 rounded-full shadow-md hover:bg-gray-100 hover:shadow-lg transition-all duration-200 cursor-pointer"
       >
         {children}
       </button>
