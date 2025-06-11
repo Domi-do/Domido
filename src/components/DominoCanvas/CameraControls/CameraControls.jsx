ce474b4c63450d270ffa0db5809d5f0920778e3d
eunseozz <eunseolovely@gmail.com>
2025-06-12 02:24:46 +0900

    Feature: 테마 선택 시 배경, 타일, 음악, 카메라 각도 적용 기능 구현
    #125

--- src/components/DominoCanvas/CameraControls/CameraControls.jsx
+++ src/components/DominoCanvas/CameraControls/CameraControls.jsx
@@ -1,9 +1,18 @@
 import { OrbitControls } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { useEffect } from "react";
 
 import useSettingStore from "@/store/useSettingStore";
 
-const CameraControls = () => {
+const CameraControls = ({ cameraAngle }) => {
   const rotationSensitivity = useSettingStore((state) => state.rotationSensitivity);
+
+  const { camera } = useThree();
+
+  useEffect(() => {
+    camera.position.set(...cameraAngle);
+    camera.lookAt(0, 0, 0);
+  }, [cameraAngle]);
 
   return (
     <OrbitControls
---
+++ src/constants/gameThema.js
@@ -0,0 +1,23 @@
+export const GAME_THEME = {
+  garden: {
+    tile: "/images/tile/dark_brown.png",
+    sound: "/sounds/garden.mp3",
+    thumbnail: "/images/thema_garden.png",
+    background: "/hdr/garden.hdr",
+    cameraAngle: [-16, 3, -12],
+  },
+  sea: {
+    tile: "/images/tile/beige.png",
+    sound: "/sounds/sea.mp3",
+    thumbnail: "/images/thema_sea.png",
+    background: "/hdr/sea.hdr",
+    cameraAngle: [-8, 3.5, -18],
+  },
+  night: {
+    tile: "/images/tile/red_brown.png",
+    sound: "/sounds/night.mp3",
+    thumbnail: "/images/thema_night.png",
+    background: "/hdr/night.hdr",
+    cameraAngle: [-12, 4, -15],
+  },
+};
