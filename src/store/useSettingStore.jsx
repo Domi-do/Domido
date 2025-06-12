3052442235721f2de0ff5eb309c924f65e149a13
eunseozz <94948796+eunseozz@users.noreply.github.com>
2025-06-12 10:18:01 +0900

    Feature: 테마 선택 기능 구현 (#126)
    * Feature: 테마 선택 기능을 위한 이미지/음악/배경 리소스 추가
    
    * Feature: 테마 선택 시 배경, 타일, 음악, 카메라 각도 적용 기능 구현

--- src/store/useSettingStore.jsx
+++ src/store/useSettingStore.jsx
@@ -1,12 +1,12 @@
 import { create } from "zustand";
 
 const useSettingStore = create((set) => ({
-  groundType: "wood_dark",
+  themaType: "sea",
   rotationSensitivity: 1,
   volumeLevel: 0.5,
   objectVolume: 3,
 
-  setGroundType: (groundType) => set(() => ({ groundType })),
+  setThemaType: (themaType) => set(() => ({ themaType })),
   setRotationSensitivity: (sensitivity) => set(() => ({ rotationSensitivity: sensitivity })),
   setVolumeLevel: (newVolume) => set(() => ({ volumeLevel: newVolume })),
   setObjectVolume: (newVolume) => set(() => ({ objectVolume: newVolume })),
