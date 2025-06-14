1706bb1ff31d6cb8162f8fee368a3f01516564a3
eunseozz <eunseolovely@gmail.com>
2025-06-15 04:56:23 +0900

    Feature: 튜토리얼 시작 시 해당 프로젝트 도미노 Clear 처리
    #133

--- src/components/DominoHUD/Tutorial/Tutorial.jsx
+++ src/components/DominoHUD/Tutorial/Tutorial.jsx
@@ -1,21 +1,28 @@
 import Intro from "@/components/DominoHUD/Tutorial/Intro/Intro";
 import StepCard from "@/components/DominoHUD/Tutorial/StepCard/StepCard";
 import { TUTORIAL_STEPS } from "@/constants/tutorialStep";
+import { useDominoMutations } from "@/hooks/Queries/useDominoMutations";
 import { useTutorialStore } from "@/store/useTutorialStore";
 
 const Tutorial = ({ onTutorialEnd }) => {
   const { currentStep, tracker, nextStep } = useTutorialStore();
+  const { mutate } = useDominoMutations();
 
   const isIntro = currentStep === 0;
   const totalStep = TUTORIAL_STEPS.length;
   const currentStepData = TUTORIAL_STEPS[currentStep - 1];
   const trackerKey = currentStepData?.trackerKey;
 
+  const handleStart = () => {
+    nextStep();
+    mutate({ dominos: [] });
+  };
+
   return (
     <>
       {isIntro ?
         <Intro
-          onStart={nextStep}
+          onStart={handleStart}
           onTutorialEnd={onTutorialEnd}
         />
       : <StepCard
