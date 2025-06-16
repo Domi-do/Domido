4f1622866925015bd7c107ff8ca977a429949c39
moonin1228 <80632139+moonin1228@users.noreply.github.com>
2025-06-16 15:19:15 +0900

    Refactor: UI 변경 및 사용자 경험 개선 작업 (#154)
    * Refactor: 자동차 오브젝트 수정
    
    * Refactor: 홈화면 이미지 프리로드 최적화
    
    * Refactor: Twist 오브젝트 제거
    
    * Refactor: 모달 창 활성 시 방향키 입력 차단

--- src/components/DominoHUD/DominoHUD.jsx
+++ src/components/DominoHUD/DominoHUD.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import ModalLayer from "@/components/Common/ModalLayer";
 import {
@@ -14,6 +14,7 @@ import { useUpdateTutorialStatus } from "@/hooks/Queries/useUpdateTutorialStatus
 import useDominoReset from "@/hooks/useDominoReset";
 import fetcher from "@/services/fetcher";
 import useDominoStore from "@/store/useDominoStore";
+import useUIStateStore from "@/store/useUIStateStore";
 import useUserStore from "@/store/useUserStore";
 import { HTTPError } from "@/utils/HTTPError";
 
@@ -23,11 +24,20 @@ const DominoHUD = ({ rigidBodyRefs, isOpenGuideToastVisible }) => {
   const [isClearConfirmModalOpen, setClearConfirmModalOpen] = useState(false);
   const [isProjectListModal, setProjectListModal] = useState(false);
   const isTutorialUser = useUserStore((state) => state.userInfo?.isTutorialUser);
+  const setHUDModalOpen = useUIStateStore((state) => state.setHUDModalOpen);
+
   const { mutate } = useUpdateTutorialStatus();
 
   const { resetDominoSimulation } = useDominoReset(rigidBodyRefs);
 
+  useEffect(() => {
+    if (isSettingModalOpen || isClearConfirmModalOpen || isProjectListModal) {
+      setHUDModalOpen(true);
+    }
+  }, [isSettingModalOpen, isClearConfirmModalOpen, isProjectListModal]);
+
   const handleCloseModal = () => {
+    setHUDModalOpen(false);
     setIsSettingModalOpen(false);
     setClearConfirmModalOpen(false);
     setProjectListModal(false);
--- src/store/useUIStateStore.jsx
+++ src/store/useUIStateStore.jsx
@@ -2,7 +2,10 @@ import { create } from "zustand";
 
 const useUIStateStore = create((set) => ({
   isCanvasReady: false,
+  isHUDModalOpen: false,
+
   setCanvasReady: () => set({ isCanvasReady: true }),
+  setHUDModalOpen: (state) => set({ isHUDModalOpen: state }),
 }));
 
 export default useUIStateStore;
