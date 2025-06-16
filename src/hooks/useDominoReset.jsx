c21b73734222031f284d3591fbd0b4e0ff5031ea
eunseozz <eunseolovely@gmail.com>
2025-06-16 19:13:32 +0900

    Fix: Reset 시 도미노의 _id 제거 후 새로 그리도록 변경하여 초기화 오류 수정
    #155

--- src/hooks/useDominoReset.jsx
+++ src/hooks/useDominoReset.jsx
@@ -1,44 +1,28 @@
 import { useEffect } from "react";
-import * as THREE from "three";
 
 import { OBJECT_GROUP_NAMES } from "@/constants/objectMetaData.js";
 import { useDominoMutations } from "@/hooks/Queries/useDominoMutations";
 import { useSocket } from "@/store/SocketContext";
 import useDominoStore from "@/store/useDominoStore";
 
-const useDominoReset = (rigidBodyRefs) => {
+const useDominoReset = () => {
   const dominos = useDominoStore((state) => state.dominos);
   const { socket, projectId } = useSocket();
   const { mutate } = useDominoMutations();
 
   const resetAllDominoes = () => {
-    const filteredDominos = dominos.filter(
-      (domino) => domino.objectInfo?.groupName === OBJECT_GROUP_NAMES.STATIC,
-    );
+    const filteredDominos = dominos
+      .filter((domino) => domino.objectInfo?.groupName === OBJECT_GROUP_NAMES.STATIC)
+      .map((domino) => {
+        const { _id, ...rest } = domino;
+        return rest;
+      });
 
     mutate({ dominos: filteredDominos });
-
-    requestAnimationFrame(() => {
-      filteredDominos.forEach((domino, index) => {
-        const rigidBody = rigidBodyRefs.current[index];
-        if (!rigidBody) return;
-
-        const { position, rotation } = domino;
-
-        const eulerRotation = new THREE.Euler(rotation[0], rotation[1], rotation[2]);
-        const rotationQuaternion = new THREE.Quaternion().setFromEuler(eulerRotation);
-
-        rigidBody.setTranslation({ x: position[0], y: position[1], z: position[2] }, true);
-        rigidBody.setRotation(rotationQuaternion, true);
-        rigidBody.setLinvel({ x: 0, y: 0, z: 0 }, true);
-        rigidBody.setAngvel({ x: 0, y: 0, z: 0 }, true);
-        rigidBody.sleep();
-      });
-    });
   };
 
   const resetDominoSimulation = () => {
-    if (!rigidBodyRefs.current.length > 0) return;
+    if (!dominos.length) return;
     resetAllDominoes();
 
     socket.emit("reset domino", { projectId });
@@ -49,8 +33,13 @@ const useDominoReset = (rigidBodyRefs) => {
       resetAllDominoes();
     });
 
+    socket.on("user joined", () => {
+      resetAllDominoes();
+    });
+
     return () => {
       socket.off("reset domino");
+      socket.off("user joined");
     };
   }, []);
 
