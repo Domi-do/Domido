8e7dff0631639dbcb4856d0067af6fadaf02e7ad
임승현 <96limshyun@gmail.com>
2025-06-16 20:55:39 +0900

    refactor: GLB 캐시용 useGlbLoader 훅 삭제
    - link preload 사용으로 삭제

--- src/pages/DominoScene.jsx
+++ src/pages/DominoScene.jsx
@@ -3,26 +3,11 @@ import { useRef } from "react";
 import DominoKeyboardHandler from "@/components/Common/DominoKeyboardHandler";
 import { DominoCanvas } from "@/components/DominoCanvas";
 import DominoHUD from "@/components/DominoHUD/DominoHUD";
-import useGlbPreloader from "@/hooks/useGlbloader";
 import useInitAchievements from "@/hooks/useInitAchievements";
 import { useStrictNavigationBlock } from "@/hooks/useStrictNavigationBlock";
 import useToastControls from "@/hooks/useToastControls";
 import { SocketProvider } from "@/store/SocketContext";
 import useUIStateStore from "@/store/useUIStateStore";
-
-const MODEL_PATHS = [
-  "/objects/beach_ball.glb",
-  "/objects/bumper.glb",
-  "/objects/cannon.glb",
-  "/objects/car.glb",
-  "/objects/lightbulb.glb",
-  "/objects/rainbowSlide.glb",
-  "/objects/slide.glb",
-  "/objects/soccer_football.glb",
-  "/objects/stairs_closed_short.glb",
-  "/objects/stairs_closed.glb",
-  "/objects/steel_ball.glb",
-];
 
 const DominoScene = () => {
   const { isOpenGuideToastVisible, openGuideToast, closeGuideToast, setIsGuideToastVisible } =
@@ -30,10 +15,6 @@ const DominoScene = () => {
 
   const rigidBodyRefs = useRef([]);
   const isCanvasReady = useUIStateStore((state) => state.isCanvasReady);
-
-  useGlbPreloader(MODEL_PATHS);
-  useStrictNavigationBlock();
-  useInitAchievements();
 
   return (
     <SocketProvider>
