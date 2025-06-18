278b7a6bc6a7851d8b6e4b3d9e012cb426a528f7
eunseozz <eunseolovely@gmail.com>
2025-06-18 15:46:53 +0900

    Test: 로직 변경에 따른 HUD 버튼 렌더링 테스트 코드 수정
    #140

--- src/components/DominoHUD/HUDButtonGroup/HUDButton.jsx
+++ src/components/DominoHUD/HUDButtonGroup/HUDButton.jsx
@@ -1,6 +1,6 @@
 import useDominoStore from "@/store/useDominoStore";
 
-const HUDButton = ({ onClick, children }) => {
+const HUDButton = ({ onClick, children, title }) => {
   const setSelectedDomino = useDominoStore((state) => state.setSelectedDomino);
 
   return (
@@ -8,6 +8,7 @@ const HUDButton = ({ onClick, children }) => {
       <button
         onMouseOver={() => setSelectedDomino(null)}
         onClick={onClick}
+        title={title}
         className="w-12 h-12 flex items-center justify-center bg-white/90 rounded-full shadow-md cursor-pointer"
       >
         {children}
